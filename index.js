64e33c52be0ae42c6f71addbea108589511bd37f
Brian <53064162+bmagee88@users.noreply.github.com>
2024-02-20 17:00:22 +0200

    mod index.js, added error handing of sort function

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 const sorter = require("./src/services/sort.service");
 const port = process.env.PORT;
+const ListEmptyException = require("./src/exceptions/ListEmptyException");
+const KeyNotExistInObjectException = require("./src/exceptions/KeyNotExistInObjectException");
+const KeyTypeNotSupportedException = require("./src/exceptions/KeyTypeNotSupportedException");
+const ListNotArrayException = require("./src/exceptions/ListNotArrayException");
+const NonObjectInListException = require("./src/exceptions/NonObjectInListException");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -35,19 +40,51 @@ app.get("/category", async (req, res) => {
     res.status(200).json(data.data);
   } catch (err) {
     console.log(err);
+    res.status(500).json("error during fetch");
   }
 });
 
 app.post("/sort", async (req, res) => {
-  if (!req.body) return res.sendStatus(400);
+  console.log("got sort")
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
   const list_to_sort = req.body.list_to_sort;
   const sort_by = req.body.sort_by;
   const order_asc = req.body.order_asc;
 
+  console.log("got order_asc")
   try {
+    console.log("got try")
     await sorter.sortByField(list_to_sort, sort_by, order_asc);
+    console.log("got sortByField")
     res.status(200).json(list_to_sort);
   } catch (err) {
-    console.log(err);
+    console.log("got err")
+    if (err instanceof ListEmptyException) {
+      console.log(err.message);
+      res.status(500).json(err.message);
+      return;
+    } else if (err instanceof KeyNotExistInObjectException) {
+      console.log(err.message);
+      res.status(500).json(err.message);
+      return;
+    } else if (err instanceof KeyTypeNotSupportedException) {
+      console.log(err.message);
+      res.status(500).json(err.message);
+      return;
+    } else if (err instanceof ListNotArrayException) {
+      console.log(err.message);
+      res.status(500).json(err.message);
+      return;
+    } else if (err instanceof NonObjectInListException) {
+      console.log(err.message);
+      res.status(500).json(err.message);
+      return;
+    } else {
+      console.log("no errors");
+    }
   }
+  
+  res.status(900).json("no errors, expected errors");
 });
