3e211f699a1d130e80b6ed6d47fb65c9b35b6fc2
Brian <53064162+bmagee88@users.noreply.github.com>
2024-02-21 10:52:10 +0200

    mod index, rm manual header access control, change name from categoryState to ImageState

--- index.js
+++ index.js
@@ -3,25 +3,28 @@ const app = express();
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const port = process.env.PORT;
-const categoryRouter = require("./src/routes/images");
-const cors = require('cors')
+const ImageRouter = require("./src/routes/images");
+const cors = require('cors');
+const ImageState = require("./src/global_data/ImageState");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors())
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Accept, Content-Type, Authorization, X-Requested-With"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Accept, Content-Type, Authorization, X-Requested-With"
+//   );
+//   next();
+// });
 
-app.use("/", categoryRouter);
+app.use("/", ImageRouter);
+// setInterval(()=>{console.log(ImageState.getImages())},5000)
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
