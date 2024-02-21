4baa7d58878edfa7fb598ce98062982bd5659545
Brian <53064162+bmagee88@users.noreply.github.com>
2024-02-21 08:35:38 +0200

    mod index, mv routing to separate folder, add cors

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const axios = require("axios");
 const bodyParser = require("body-parser");
-const sorter = require("./src/services/sort.service");
 const port = process.env.PORT;
-const ListEmptyException = require("./src/exceptions/ListEmptyException");
-const KeyNotExistInObjectException = require("./src/exceptions/KeyNotExistInObjectException");
-const KeyTypeNotSupportedException = require("./src/exceptions/KeyTypeNotSupportedException");
-const ListNotArrayException = require("./src/exceptions/ListNotArrayException");
-const NonObjectInListException = require("./src/exceptions/NonObjectInListException");
+const categoryRouter = require("./src/routes/images");
+const cors = require('cors')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(cors())
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -24,62 +21,8 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.use("/", categoryRouter);
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
-});
-
-app.get("/category", async (req, res) => {
-  const CATEGORY = req.query.category;
-  const PAGE = req.query.page;
-  const PER_PAGE = req.query.per_page;
-  const KEY = process.env.KEY;
-  try {
-    const data = await axios.get(
-      `https://pixabay.com/api/?key=${KEY}&q=${CATEGORY}&page=${PAGE}&per_page=${PER_PAGE}`
-    );
-    res.status(200).json(data.data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json("error during fetch");
-  }
-});
-
-app.post("/sort", async (req, res) => {
-  if (!req.body) {
-    return res.sendStatus(400);
-  }
-  const list_to_sort = req.body.list_to_sort;
-  const sort_by = req.body.sort_by;
-  const order_asc = req.body.order_asc;
-
-  try {
-    await sorter.sortByField(list_to_sort, sort_by, order_asc);
-    res.status(200).json(list_to_sort);
-  } catch (err) {
-    if (err instanceof ListEmptyException) {
-      console.log(err.message);
-      res.status(500).json(err.message);
-      return;
-    } else if (err instanceof KeyNotExistInObjectException) {
-      console.log(err.message);
-      res.status(500).json(err.message);
-      return;
-    } else if (err instanceof KeyTypeNotSupportedException) {
-      console.log(err.message);
-      res.status(500).json(err.message);
-      return;
-    } else if (err instanceof ListNotArrayException) {
-      console.log(err.message);
-      res.status(500).json(err.message);
-      return;
-    } else if (err instanceof NonObjectInListException) {
-      console.log(err.message);
-      res.status(500).json(err.message);
-      return;
-    } else {
-      console.log("no errors");
-    }
-  }
-  
-  res.status(900).json("no errors, expected errors");
 });
