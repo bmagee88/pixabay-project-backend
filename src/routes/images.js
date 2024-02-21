eabef834f9c462056fc96cc5e9533394a0ae5046
Brian <53064162+bmagee88@users.noreply.github.com>
2024-02-21 10:54:56 +0200

    mod image.js, made per_page on server, calcualted remaining images, modify return object, sort from cache instead of through network

--- src/routes/images.js
+++ src/routes/images.js
@@ -9,20 +9,24 @@ const ListNotArrayException = require("../exceptions/ListNotArrayException");
 const NonObjectInListException = require("../exceptions/NonObjectInListException");
 const sorter = require("../services/sort.service");
 
+const PER_PAGE = 9;
 
 router.get("/category", async (req, res) => {
   let CATEGORY = req.query.category;
   const KEY = process.env.KEY;
-  const PER_PAGE = req.query.per_page;
-  const PAGE = req.query.page - 1;
+  const PAGE = +req.query.page - 1;
   const OFFSET = PAGE * PER_PAGE;
   const category_state = ImageState.getKey(CATEGORY);
   if (category_state) {
-    console.log(
-      "cached hit",
-      category_state.slice(OFFSET, OFFSET + PER_PAGE)
-    );
-    res.status(200).json(category_state.slice(OFFSET, OFFSET + PER_PAGE));
+    console.log("cached hit", category_state.slice(OFFSET, OFFSET + PER_PAGE));
+    const remaining_pre = category_state.length - (OFFSET + PER_PAGE);
+    const remaining = remaining_pre <= 0 ? 0 : remaining_pre;
+    const response = {
+      isSuccess: true,
+      remaining,
+      data: category_state.slice(OFFSET, OFFSET + PER_PAGE),
+    };
+    res.status(200).json(response);
     return;
   }
 
@@ -33,50 +37,78 @@ router.get("/category", async (req, res) => {
     );
     ImageState.setImages(CATEGORY, data.data.hits);
     console.log("new category", ImageState.getImages());
-    res.status(200).json(data.data.hits.slice(0, PER_PAGE));
+
+    const category_state = ImageState.getKey(CATEGORY);
+    const remaining_pre = category_state.length - (OFFSET + PER_PAGE);
+    const remaining = remaining_pre <= 0 ? 0 : remaining_pre;
+    const result = data.data.hits.slice(0, PER_PAGE);
+    const response = { isSuccess: true, remaining, data: result };
+
+    res.status(200).json(response);
   } catch (err) {
     console.log(err);
     res.status(500).json("error during fetch");
   }
 });
 
-
 router.post("/sort", async (req, res) => {
-    if (!req.body) {
-      return res.sendStatus(400);
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+  const sort_by = req.body.sort_by;
+  const order_asc = req.body.order_asc;
+  const CATEGORY = req.body.category;
+  const PAGE = req.body.page - 1;
+  const OFFSET = PAGE * PER_PAGE;
+  const category_state = ImageState.getKey(CATEGORY);
+  const remaining_pre =
+    category_state.length - (OFFSET + PER_PAGE);
+  const remaining = remaining_pre <= 0 ? 0 : remaining_pre;
+
+  console.log(`sort-${sort_by}, order-${order_asc}`);
+
+  let  list_to_sort = [];
+  if (category_state) {
+    list_to_sort = category_state.slice(OFFSET, OFFSET + PER_PAGE)
+  }
+
+  if (sort_by === undefined || order_asc === undefined) {
+    const response = { data: list_to_sort, remaining, isSuccess: false };
+    res.status(400).json(response);
+    return;
+  }
+
+  try {
+    await sorter.sortByField(list_to_sort, sort_by, order_asc);
+
+    const response = { isSuccess: true, remaining, data: list_to_sort };
+
+    res.status(200).json(response);
+  } catch (err) {
+    if (err instanceof ListEmptyException) {
+      console.log(err.message);
+      res.status(500).json(err.message);
+      return;
+    } else if (err instanceof KeyNotExistInObjectException) {
+      console.log(err.message);
+      res.status(500).json(err.message);
+      return;
+    } else if (err instanceof KeyTypeNotSupportedException) {
+      console.log(err.message);
+      res.status(500).json(err.message);
+      return;
+    } else if (err instanceof ListNotArrayException) {
+      console.log(err.message);
+      res.status(500).json(err.message);
+      return;
+    } else if (err instanceof NonObjectInListException) {
+      console.log(err.message);
+      res.status(500).json(err.message);
+      return;
+    } else {
+      console.log("no errors");
     }
-    const list_to_sort = req.body.list_to_sort;
-    const sort_by = req.body.sort_by;
-    const order_asc = req.body.order_asc;
-  
-    try {
-      await sorter.sortByField(list_to_sort, sort_by, order_asc);
-      res.status(200).json(list_to_sort);
-    } catch (err) {
-      if (err instanceof ListEmptyException) {
-        console.log(err.message);
-        res.status(500).json(err.message);
-        return;
-      } else if (err instanceof KeyNotExistInObjectException) {
-        console.log(err.message);
-        res.status(500).json(err.message);
-        return;
-      } else if (err instanceof KeyTypeNotSupportedException) {
-        console.log(err.message);
-        res.status(500).json(err.message);
-        return;
-      } else if (err instanceof ListNotArrayException) {
-        console.log(err.message);
-        res.status(500).json(err.message);
-        return;
-      } else if (err instanceof NonObjectInListException) {
-        console.log(err.message);
-        res.status(500).json(err.message);
-        return;
-      } else {
-        console.log("no errors");
-      }
-    }
+  }
 });
 
 // Original implementation fetching using pagination via pixabay api
