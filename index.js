20604fa3e5363163ec46a9dc2d17acd1c5330b86
Brian <53064162+bmagee88@users.noreply.github.com>
2024-02-21 10:52:50 +0200

    mod index, rm manual access control, change categorystate to image state

--- index.js
+++ index.js
@@ -12,16 +12,6 @@ app.use(bodyParser.json());
 
 app.use(cors())
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Accept, Content-Type, Authorization, X-Requested-With"
-//   );
-//   next();
-// });
-
 app.use("/", ImageRouter);
 // setInterval(()=>{console.log(ImageState.getImages())},5000)
 
